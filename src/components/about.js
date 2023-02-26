f1c17f7c24c4a9f3cdcbf81bd6bf51004fe7a287
function07 <houssine199222@gmail.com>
2023-02-26 15:24:24 -0500

    Add JS files for base webpage and shop menu

--- src/components/about.js
+++ src/components/about.js
@@ -1,35 +1,6 @@
-const selectContent = document.querySelector("#content");
-
-const createNavBar = () => {
-  const menuArray = ["About", "Menu", "Contact"];
-
-  // Creates the base navigation bar
-  const navBar = document.createElement("div");
-  navBar.setAttribute("id", "navbar");
-  selectContent.appendChild(navBar);
-
-  // Creates the Name of the Tea Shop
-  const name = document.createElement("div");
-  name.setAttribute("id", "name");
-  name.textContent = "London Fog";
-  navBar.appendChild(name);
-
-  // Creates the base menu
-  const menu = document.createElement("ul");
-  menu.setAttribute("id", "menu");
-  navBar.appendChild(menu);
-
-  // Creates menu selections
-  for (let i = 0; i < menuArray.length; i++) {
-    const list = document.createElement("li");
-
-    list.textContent = menuArray[i];
-    menu.appendChild(list);
-  }
-};
-
 const createAbout = () => {
   // Creates base about section
+  const selectContent = document.querySelector("#content");
   const about = document.createElement("div");
   about.setAttribute("id", "about");
   selectContent.appendChild(about);
@@ -48,11 +19,4 @@ const createAbout = () => {
     "We are committed to serve the best quality tea to our customers and fresh-baked pastries";
 };
 
-const createCopyright = () => {
-  const copyright = document.createElement("div");
-  copyright.setAttribute("id", "credits");
-  copyright.textContent = "Copyright © 2023 function09";
-  selectContent.appendChild(copyright);
-};
-
-export { createNavBar, createAbout, createCopyright };
+export { createAbout };
