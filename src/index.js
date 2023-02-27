233f2ff1326d06becaa0b6f0ed9661d5c50414b2
function07 <houssine199222@gmail.com>
2023-02-26 23:34:24 -0500

    Update index.js withtab-switching feature

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 import "./style.css";
 
-import { createNavBar, createAbout, createCopyright } from "./components/about";
+import { createNavBar, createCopyright } from "./components/UI";
+import { createAbout } from "./components/about";
+import { createMenu } from "./components/menu";
+import { createContact } from "./components/contact";
 
 createNavBar();
 createAbout();
 createCopyright();
+
+const selectMenuSelection = document.querySelectorAll(".menuSelection");
+
+selectMenuSelection.forEach((selection) => {
+  selection.addEventListener("click", (e) => {
+    const selectValue = e.target.dataset.value;
+
+    switch (selectValue) {
+      case "item-0":
+        createAbout();
+        break;
+      case "item-1":
+        createMenu();
+        break;
+      case "item-2":
+        createContact();
+        break;
+    }
+  });
+});
